cf2db991356e4e6abe2193a58946602fb4c7e904
Li Yichen <nj_lyc@163.com>
2021-04-18 19:35:51 +0800

    New

---
+++ src/player random.py
@@ -0,0 +1,17 @@
+import random
+from GameMap import GameMap
+def player_func(map_info:GameMap, player_id:int):
+    actions=[]
+    for i in range(1,5):
+        if map_info.nodes[i].power[player_id] > 0:
+            can = map_info.nodes[i].get_next()
+            chosen=[]
+            for n in range(random.randint(1,len(can))):
+                chosen.append(random.choice(can))
+            p = []
+            for x in range(len(chosen)):
+                p.append(random.random() * map_info.nodes[i].power[player_id] / len(chosen))
+            for y in range(len(chosen)):
+                actions.append((i,chosen[y],p[y]))
+    print(actions)
+    return actions
