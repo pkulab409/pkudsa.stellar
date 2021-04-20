6417b2e87a86caa5fcb479b66a2f497ed71140d5
xhzgenius <82707775+xhzgenius@users.noreply.github.com>
2021-04-21 01:14:55 +0800

    4.21凌晨版本。这一版是究极整合版。集合了4.20大家所有的更新，并附上了新的随机生成地图函数。
    4.21凌晨版本。这一版是究极整合版。集合了4.20大家所有的更新，并附上了新的随机生成地图函数，位于HexagonForce.py里面。（老几何冲刺玩家了
    里面的Generate_Hexagon(a:int, p_node:int, p_edge:int)返回的是一个字典，可直接用于main函数。
    并且写了判断连通性的代码！保证生成的地图一定连通！！！

--- src/main.py
+++ src/main.py
@@ -4,13 +4,14 @@ from GameCore import Game
 from MapDesign import g_design
 from config import MAX_TIME, MAX_TURN
 from DesignGenerator import DesignGenerator
+from HexagonForce import Generate_Hexagon
 
 cnt = [0, 0]
 
 if __name__ == '__main__':
-    for i in range(10):
-        dg = DesignGenerator(bridge=0.8, branch=(2, 4), depth=3)
-        g = Game('player1', 'player2', MAX_TIME, MAX_TURN, dg.generate())
+    for i in range(1):
+        # dg = DesignGenerator(bridge=0.8, branch=(2, 4), depth=3)
+        g = Game('player1', 'player2', MAX_TIME, MAX_TURN, Generate_Hexagon(4, 0.25, 0.50))
 
         fuck = g.run()
         if fuck == "player1":
