63939707d645973346655bcd73a153e100b91c34
xhzgenius <82707775+xhzgenius@users.noreply.github.com>
2021-05-25 14:40:17 +0800

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -8,13 +8,13 @@ from HexagonForce import Generate_Hexagon
 
 cnt = [0, 0]
 #player_file1, player_flie2 = sys.argv[1], sys.argv[2]
-player_file1, player_flie2 = "stupidAI", "stupidAI"
+player_file1, player_flie2 = "XHZAI一代", "XHZAI二点五代"
 print(player_file1, player_flie2)
 
 if __name__ == '__main__':
     for i in range(1):
         # dg = DesignGenerator(bridge=0.8, branch=(2, 4), depth=3)
-        g = Game(player_file1, player_flie2, Generate_Hexagon(4, 0.20, 0.20))
+        g = Game(player_file1, player_flie2, Generate_Hexagon(7, 0.35, 0.10))
 
         fuck = g.run()
         if fuck == 0:
